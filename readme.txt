1c54c92be4192332b79d481088fa9a47731513ad
DaLei Sun <sundalei1988@gmail.com>
2020-12-03 14:17:12 +0800

    'test'

---
+++ readme.txt
@@ -0,0 +1,3 @@
+https://www.baeldung.com/executable-jar-with-maven
+https://winterbe.com/posts/2018/08/29/migrate-maven-projects-to-java-11-jigsaw/
+https://www.selenium.dev/documentation/en/webdriver/driver_requirements/
---
+++ src/main/java/demo/App.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        WebDriver driver = new ChromeDriver();
+        System.out.println(driver);
+    }
+}
