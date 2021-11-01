13899b2a4c96a2fd03c8040b7cf31a2ece567c74
sundalei <sundalei1988@gmail.com>
2021-11-01 11:07:26 +0800

    test

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,13 +1,27 @@
 package demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("hello");
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class App {
+
+    public static void main(String[] args) {
+        try (InputStream in = App.class.getClassLoader().getResourceAsStream("mssql.properties")) {
+            Properties props = new Properties();
+
+            if (in == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+
+            props.load(in);
+
+            System.out.println(props.getProperty("url"));
+            System.out.println(props.getProperty("username"));
+            System.out.println(props.getProperty("password"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/demo/InsertDataInfo.java
+++ src/main/java/demo/InsertDataInfo.java
@@ -1,59 +1,74 @@
 package demo;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class InsertDataInfo {
 
     static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=demo2;integratedSecurity=true";
-
 
     public static void main(String[] args) {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        
-        try {
-            Class.forName(JDBC_DRIVER);
 
-            String insertSql = "INSERT INTO dbo.storage_file (name, last_modified, created_at) VALUES (?, getutcdate(), getutcdate())";
+        try (InputStream in = InsertDataInfo.class.getClassLoader().getResourceAsStream("mssql.properties")) {
 
-            connection = DriverManager.getConnection(DB_URL);
-            statement = connection.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
+            Properties props = new Properties();
+            props.load(in);
 
-            int col = 1;
-            statement.setString(col++, "hello_insert");
+            var url = props.getProperty("url");
+            var username = props.getProperty("username");
+            var password = props.getProperty("password");
 
-            var rows = statement.executeUpdate();
-            statement.executeUpdate();
-
-            if (rows == 1) {
-                ResultSet rs = statement.getGeneratedKeys();
-                
-                if (rs != null && rs.next()) {
-                    System.out.println(rs.getInt(1));
-                }
-            }
-
-            connection.close();
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
+            Connection connection = null;
+            PreparedStatement statement;
 
             try {
-                if (connection != null) {
-                    connection.close();
+                Class.forName(JDBC_DRIVER);
+
+                String insertSql = "INSERT INTO storage_file (name, last_modified, created_at)" +
+                        " VALUES (?, getutcdate(), getutcdate())";
+
+                connection = DriverManager.getConnection(url, username, password);
+                statement = connection.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
+
+                int col = 1;
+                statement.setString(col++, "hello_insert");
+
+                var rows = statement.executeUpdate();
+                statement.executeUpdate();
+
+                if (rows == 1) {
+                    ResultSet rs = statement.getGeneratedKeys();
+
+                    if (rs != null && rs.next()) {
+                        System.out.println(rs.getInt(1));
+                    }
                 }
+
+                connection.close();
             } catch (SQLException se) {
                 se.printStackTrace();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+
+                try {
+                    if (connection != null) {
+                        connection.close();
+                    }
+                } catch (SQLException se) {
+                    se.printStackTrace();
+                }
             }
+            System.out.println("Goodbye!");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("Goodbye!");
     }
 }
